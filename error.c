8af5d1f75731794ca746da93a48b2dd9dda93ffb
Mahmoud Al-Adawy <m.s.el-din@hotmail.com>
2023-08-21 21:25:11 -0700

    check betty for error.c

--- error.c
+++ error.c
@@ -2,7 +2,7 @@
 
 /**
  * handle_interrupt - Signal handler for the CTRL+C signal.
- * @signal: The signal number.
+ * @input_signal: The signal number.
  */
 void handle_interrupt(__attribute__((unused)) int input_signal)
 {
@@ -22,43 +22,43 @@ void printError(char *file_id, char *input, int errorNum)
 {
 	char *str;
 
-	if (errorNum == 127) /* Sorry, command not found */
+	if (errorNum == 127)	/* Sorry, command not found */
 	{
 		write(STDOUT_FILENO, file_id, str_len(file_id));
 		write(STDOUT_FILENO, ": 1: ", 5);
 		write(STDOUT_FILENO, input, str_len(input));
 		write(STDOUT_FILENO, ": not found!!\n", 12);
 	}
-	if (errorNum == 2) /* any syntax error*/
+	if (errorNum == 2)	/* any syntax error*/
 	{
 		str = "sh: 1: there are some Syntax error: \"";
 		write(STDOUT_FILENO, str, str_len(str));
-        write(STDOUT_FILENO, input, str_len(input));
+		write(STDOUT_FILENO, input, str_len(input));
 		write(STDOUT_FILENO, ";", 1);
 		str = "\" unexpected...\n";
 		write(STDOUT_FILENO, str, str_len(str));
 	}
-	if (errorNum == 3) /* Memory allocation error */
+	if (errorNum == 3)	/* Memory allocation error */
 	{
 		write(STDOUT_FILENO, file_id, str_len(file_id));
 		write(STDOUT_FILENO, ": 1: ", 5);
 		str = "Sorry, Internal error allocating memory\n";
 		write(STDOUT_FILENO, str, str_len(str));
 	}
-    if (errorNum == 4) /* Invalid argument */
-    {
+	if (errorNum == 4)	/* Invalid argument */
+	{
 	write(STDOUT_FILENO, file_id, strlen(file_id));
 	write(STDOUT_FILENO, ": 1: ", 5);
 	str = "Sorry, Invalid argument: ";
 	write(STDOUT_FILENO, str, str_len(str));
 	write(STDOUT_FILENO, input, str_len(input));
 	write(STDOUT_FILENO, "\n", 1);
-    }
-    if (errorNum == 5) /* File not found */
-    {
+	}
+	if (errorNum == 5)	/* File not found */
+	{
 	write(STDOUT_FILENO, "Error: File not found: ", 23);
 	write(STDOUT_FILENO, input, strlen(input));
 	write(STDOUT_FILENO, "\n", 1);
-    }
+	}
 }
 
